ff8fa8a003cf38c3c88d35174ac57de714de0e3b
JAYARAKKINI <37235960+JAYARAKKINI@users.noreply.github.com>
2020-02-26 12:55:22 +0530

    Create task1.js

---
+++ 25-02-2020/task1.js
@@ -0,0 +1,35 @@
+# To convert country name into capital letters using map function
+
+const countryname=dummy.map((item)=>{
+    return(item.name).toUpperCase()})
+console.log(countryname)
+
+# To print capital of country length 5 and less than 5 using filter
+
+const caplen=dummy.filter((item)=>{
+    return(item.capital).length<=5})
+console.log(caplen)
+
+# To find the specific country name using find
+
+const finditem=dummy.find((item)=>{
+    return item.name==='India'})
+console.log(finditem)
+
+# To sum the all no's in array using reduce
+
+var sum=[1,2,3,4,5,6,7,8,9,10]
+var sum =sum.reduce(function(accumulator,currentvalue,index){
+    return accumulator+currentvalue;
+});
+console.log(sum)
+
+# To print odd numbers
+
+var a=[1,2,3,4,5,6,7,8,9,10]
+var sum=a.reduce(function(accumulator,currentvalue,index){
+    if(currentvalue%2!=0){
+        return accumulator+currentvalue;}
+    return accumulator
+});
+console.log(sum)
