dc0bb76b0b3d9f9cf43dfc901ae843c76176f138
JAYARAKKINI <37235960+JAYARAKKINI@users.noreply.github.com>
2020-02-28 14:24:45 +0530

    Create 6_area_of_equilateral_tri.js

---
+++ 28_02_2020/6_area_of_equilateral_tri.js
@@ -0,0 +1,6 @@
+const readline=require('readline');
+const inp=readline.createInterface({input:process.stdin});
+inp.on("line",(data)=>{
+  area=(Math.sqrt(3)/4)*(data*data);
+  console.log(area.toFixed(2));
+});
