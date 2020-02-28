78fb2d77710e3ef56937fcdcfcaaa6860eb003a8
JAYARAKKINI <37235960+JAYARAKKINI@users.noreply.github.com>
2020-02-28 11:58:58 +0530

    Create 1_sum.js

---
+++ 28_02_2020/1_sum.js
@@ -0,0 +1,10 @@
+const readline=require('readline');
+const inp=readline.createInterface({input:process.stdin})
+const n=[]
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",(data)=>{
+    var a=n[0];
+	var b=n[1];
+    console.log(parseInt(a)+parseInt(b));
+});
