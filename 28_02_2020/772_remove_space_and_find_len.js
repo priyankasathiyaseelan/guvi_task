d521ff594e27ebe9262c735186bc0cdb78dd69aa
JAYARAKKINI <37235960+JAYARAKKINI@users.noreply.github.com>
2020-03-05 11:15:15 +0530

    Create 772_remove_space_and_find_len.js

---
+++ 28_02_2020/772_remove_space_and_find_len.js
@@ -0,0 +1,6 @@
+const readline = require('readline');
+const inp=readline.createInterface({input:process.stdin});
+inp.on("line",(data)=>{
+  data=data.replace(/\s+/g,"");
+  console.log(data.length);
+});
